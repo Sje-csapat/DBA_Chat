6cafd0d9638d036e81c1e0e75e5a2c46c221eea4
Salmeszpapi <salma638998@gmail.com>
2020-12-26 00:47:18 +0100

    Jelszo hasheles

--- masodik/Controllers/HomeController.cs
+++ masodik/Controllers/HomeController.cs
@@ -6,6 +6,8 @@ using System.Linq;
 using System.Threading.Tasks;
 using masodik.Models;
 using System.Data.SQLite;
+using System.Security.Cryptography;
+using Microsoft.AspNetCore.Cryptography.KeyDerivation;
 
 namespace masodik
 {
@@ -13,9 +15,10 @@ namespace masodik
     {
 
         // GET: HomeController
-        [Route("")]
+        
         public ActionResult Index()
         {
+
             System.Diagnostics.Debug.WriteLine(!string.IsNullOrEmpty(HttpContext.Session.GetString("is_logged_in")));
             //return View("Session");
             if (!string.IsNullOrEmpty(HttpContext.Session.GetString("is_logged_in")))
@@ -23,9 +26,11 @@ namespace masodik
                 //redirect or view
                 return View("Session");
             }
-            else {
+            else
+            {
                 return RedirectToAction(nameof(Login));
             }
+            
         }
 
         [Route("login")]
@@ -58,6 +63,7 @@ namespace masodik
         }
         public ActionResult Log(string username, string password)
         {
+            
             Home passdata = new Home
             {
                 username = username,
@@ -66,7 +72,7 @@ namespace masodik
             
             SQLiteConnection dbconn = Globals.Dbconn;
             dbconn.Open();
-            System.Diagnostics.Debug.WriteLine(username);
+            //System.Diagnostics.Debug.WriteLine(username);
 
             using var cmd = new SQLiteCommand(dbconn);
             cmd.CommandText = "SELECT * FROM users WHERE `username` = @username";
@@ -79,28 +85,46 @@ namespace masodik
             {
                 //System.Diagnostics.Debug.WriteLine($"{rdr.GetInt32(0)} {rdr.GetString(1)} {rdr.GetString(2)}");
                 string db_password = rdr.GetString(2);
-                if (db_password == password) {
+                bool pwd_verify = Globals.VerifyPassword(db_password, password);
+                if (pwd_verify) {
                     ViewBag.Message = passdata;
                     HttpContext.Session.SetString("is_logged_in", "1");
                     HttpContext.Session.SetString("user_id", rdr.GetInt32(0).ToString());
                     HttpContext.Session.SetString("user_username", rdr.GetString(1).ToString());
                     isLoggedIn = 1;
+                    Globals.Logger(rdr.GetInt32(0), "Login succes");
                 } else
                 {
                     //password mismatch
+
+                    Globals.Logger(rdr.GetInt32(0), "Failed login - password mismatch");
                     ViewBag.Message = passdata;
                     isLoggedIn = 0;
                 }
             }
             dbconn.Close();
             ViewBag.Message = passdata;
-            return View("Proba");
+            if (1==isLoggedIn)
+            {
+                return View("Proba");
+            }
+            else
+            {
+                return View("Login");
+            }
+            
         }
         public ActionResult Reg(string username, string password, string password2)
         {
+            string hashed = Globals.HashPassword(password, null, false);
+            
+
+            bool visszafejtve = Globals.VerifyPassword(hashed, "KIKasdI");
+            System.Diagnostics.Debug.WriteLine(visszafejtve);
+
             Register passdata = new Register
             {
-                username = username,
+                username = hashed,
                 password = password,
                 password2 = password2
             };
@@ -113,7 +137,7 @@ namespace masodik
             using var cmd = new SQLiteCommand(dbconn);
             cmd.CommandText = "INSERT INTO users (username, password, created_at) values(@username, @password, @created_at)";
             cmd.Parameters.AddWithValue("@username", username);
-            cmd.Parameters.AddWithValue("@password", password);
+            cmd.Parameters.AddWithValue("@password", hashed);
             cmd.Parameters.AddWithValue("@created_at", (string)(DateTime.Now.Subtract(new DateTime(1970, 1, 1))).TotalSeconds.ToString());
             cmd.Prepare();
             cmd.ExecuteNonQuery();
@@ -123,5 +147,6 @@ namespace masodik
             ViewBag.Message = passdata;
             return View("Proba");
         }
+        
     }
 }
--- masodik/Globals.cs
+++ masodik/Globals.cs
@@ -1,8 +1,11 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Cryptography.KeyDerivation;
+using System;
 using System.Collections.Generic;
 using System.Data.SQLite;
 using System.IO;
 using System.Linq;
+using System.Data;
+using System.Security.Cryptography;
 using System.Threading.Tasks;
 
 namespace masodik
@@ -10,6 +13,7 @@ namespace masodik
     public static class Globals
     {
         public static SQLiteConnection Dbconn { get; set; }
+        public static SQLiteConnection Dbasdadsconn { get; set; }
         public static void TestDBConnection(string contentRoot)
         {
             string dabasestr = contentRoot + "/database.db";
@@ -26,6 +30,66 @@ namespace masodik
                 System.Diagnostics.Debug.WriteLine("Database not found, shutting down!");
                 Environment.Exit(1);
             }
+        }
+        public static void Logger(int user_id,string message)
+        {
+            var dbconn = Globals.Dbconn;
+            int conn_status = 0;
+            if(dbconn != null && dbconn.State == ConnectionState.Closed)
+            {
+                conn_status = 1;
+                dbconn.Open();
+            }
+            using var cmd = new SQLiteCommand(dbconn); 
+            cmd.CommandText = "INSERT INTO Log(user_id,message) values(@user_id, @message)";
+            cmd.Parameters.AddWithValue("@user_id", user_id.ToString());
+            cmd.Parameters.AddWithValue("@message", message);
+            cmd.Prepare();
+            cmd.ExecuteNonQuery();
+            if(Convert.ToBoolean(conn_status))
+            {
+                dbconn.Close();
+            }
+        }
+        public static string HashPassword(string password, byte[] salt = null, bool needsOnlyHash = false)
+        {
+            if (salt == null || salt.Length != 16)
+            {
+                // generate a 128-bit salt using a secure PRNG
+                salt = new byte[128 / 8];
+                using (var rng = RandomNumberGenerator.Create())
+                {
+                    rng.GetBytes(salt);
+                }
+            }
+            string hashed = Convert.ToBase64String(KeyDerivation.Pbkdf2(
+               password: password,
+               salt: salt,
+               prf: KeyDerivationPrf.HMACSHA256,
+               iterationCount: 10000,
+               numBytesRequested: 256 / 8));
+
+            if (needsOnlyHash) return hashed;
+            // password will be concatenated with salt using ':'
+            return $"{hashed}:{Convert.ToBase64String(salt)}";
+        }
+        public static bool VerifyPassword(string hashedPasswordWithSalt, string passwordToCheck)
+        {
+            // retrieve both salt and password from 'hashedPasswordWithSalt'
+            var passwordAndHash = hashedPasswordWithSalt.Split(':');
+            if (passwordAndHash == null || passwordAndHash.Length != 2)
+                return false;
+            var salt = Convert.FromBase64String(passwordAndHash[1]);
+            if (salt == null)
+                return false;
+            // hash the given password
+            var hashOfpasswordToCheck = HashPassword(passwordToCheck, salt, true);
+            // compare both hashes
+            if (String.Compare(passwordAndHash[0], hashOfpasswordToCheck) == 0)
+            {
+                return true;
+            }
+            return false;
         }
     }
 }
